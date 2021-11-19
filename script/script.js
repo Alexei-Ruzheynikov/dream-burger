9c227ba39d248937ba95cc29bad72ce14383802d
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-11-19 19:27:54 +0300

    Разработал возможность нескольких вопросов(слайдов) и анимацию

--- script/script.js
+++ script/script.js
@@ -35,6 +35,86 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerBtn = document.getElementById("burger");
   burgerBtn.style.display = "none";
 
+  const nextButton = document.querySelector("#next");
+  const prevButton = document.querySelector("#prev");
+
+  const modalDialog = document.querySelector(".modal-dialog");
+
+  const questions = [
+    {
+      question: "Какого цвета бургер?",
+      answers: [
+        {
+          title: "Стандарт",
+          url: "./image/burger.png"
+        },
+        {
+          title: "Черный",
+          url: "./image/burgerBlack.png"
+        }
+      ],
+      type: "radio"
+    },
+    {
+      question: "Из какого мяса котлета?",
+      answers: [
+        {
+          title: "Курица",
+          url: "./image/chickenMeat.png"
+        },
+        {
+          title: "Говядина",
+          url: "./image/beefMeat.png"
+        },
+        {
+          title: "Свинина",
+          url: "./image/porkMeat.png"
+        }
+      ],
+      type: "radio"
+    },
+    {
+      question: "Дополнительные ингредиенты?",
+      answers: [
+        {
+          title: "Помидор",
+          url: "./image/tomato.png"
+        },
+        {
+          title: "Огурец",
+          url: "./image/cucumber.png"
+        },
+        {
+          title: "Салат",
+          url: "./image/salad.png"
+        },
+        {
+          title: "Лук",
+          url: "./image/onion.png"
+        }
+      ],
+      type: "checkbox"
+    },
+    {
+      question: "Добавить соус?",
+      answers: [
+        {
+          title: "Чесночный",
+          url: "./image/sauce1.png"
+        },
+        {
+          title: "Томатный",
+          url: "./image/sauce2.png"
+        },
+        {
+          title: "Горчичный",
+          url: "./image/sauce3.png"
+        }
+      ],
+      type: "radio"
+    }
+  ];
+
   let clientWidth = document.documentElement.clientWidth;
 
   if (clientWidth < 768) {
@@ -59,7 +139,21 @@ document.addEventListener("DOMContentLoaded", function () {
     playTest();
   });
 
+  let count = -100;
+  modalDialog.style.top = "-100%";
+
+  const animateModal = () => {
+    modalDialog.style.top = count + "%";
+    count += 3;
+    if (count < 0) {
+      requestAnimationFrame(animateModal);
+    } else {
+      count = -100;
+    }
+  };
+
   btnOpenModal.addEventListener("click", () => {
+    requestAnimationFrame(animateModal);
     modalBlock.classList.add("d-block");
     playTest();
   });
@@ -81,22 +175,41 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const playTest = () => {
-    const renderQuestions = () => {
-      questionTitle.textContent = "Какого цета бургер вы хотите?";
-      const name = "Стандарт";
-      const img = "./image/burger.png";
-
-      formAnswers.innerHTML = `
-        <div class="answers-item d-flex flex-column">
-            <input type="radio" id="answerItem1" name="answer" class="d-none">
-            <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                <img class="answerImg" src="${img}" alt="burger">
-                <span>${name}</span>
+    let numberQuestion = 0;
+    const renderAnswers = (index) => {
+      questions[index].answers.forEach((answer) => {
+        const answerItem = document.createElement("div");
+        answerItem.classList.add(
+          "answers-item",
+          "d-flex",
+          "justify-content-center"
+        );
+        answerItem.innerHTML = `
+            <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+            <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                <img class="answerImg" src="${answer.url}" alt="burger">
+                <span>${answer.title}</span>
             </label>
-        </div>
-           
-      `;
+        `;
+        formAnswers.appendChild(answerItem);
+      });
     };
-    renderQuestions();
+
+    const renderQuestions = (indexQuestion) => {
+      formAnswers.innerHTML = "";
+      questionTitle.textContent = `${questions[indexQuestion].question}`;
+      renderAnswers(indexQuestion);
+    };
+    renderQuestions(numberQuestion);
+
+    nextButton.onclick = () => {
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+    };
+
+    prevButton.onclick = () => {
+      numberQuestion--;
+      renderQuestions(numberQuestion);
+    };
   };
 });
